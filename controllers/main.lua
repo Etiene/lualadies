4bd0e845b2554def2993df799c9f426cc6e2b2fc
Etiene Dalcol <dalcol@etiene.net>
2014-12-08 15:14:54 +0100

    Hit counter

--- controllers/main.lua
+++ controllers/main.lua
@@ -1,11 +1,24 @@
 local main = {}
 
+function hit()
+	local file = io.input ("hit.txt")
+	local n = file:read("*n")
+	file:close()
+	file = io.output ("hit.txt")
+	file:write(n+1)
+	file:close()
+end
+
 function main.index(page)
+	hit()
     page:render('index')
 end
 
 function main.events(page)
+	hit()
     page:render('events')
 end
+
+
 
 return main
--- index.lua
+++ index.lua
@@ -1,2 +1,6 @@
 require "sailor"
+
+
+file = io.open ("hit.txt","r+")
+
 sailor.launch()
